0b5539d7e58467718eba2ac2ae3e39d14688e33d
Abhinav Palacharla <abhinav.palacharla@gmail.com>
2022-06-08 23:37:27 -0700

    ✨ Added ability to switch between grid and list view

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -1,19 +1,37 @@
-import { Grid, ActionPanel, Action, Icon } from "@raycast/api";
+import { Grid, List, ActionPanel, Action, Icon, popToRoot } from "@raycast/api";
 import { Team } from "../types/standings.types";
 import Roster from "../views/roster";
 
 type PropTypes = {
   team: Team;
+  viewType: "list" | "grid";
 };
 
-const TeamComponent = ({ team }: PropTypes) => {
+const GridTeamComponent = ({ team }: { team: Team }) => {
   return (
     <Grid.Item
       key={team.id}
       title={team.name}
       content={team.logo}
       subtitle={`W: ${team.wins} • L: ${team.losses}`}
-      // accessories={[{ text: `W: ${team.wins}` }, { text: `L: ${team.losses}` }]}
+      actions={
+        <ActionPanel>
+          <Action.Push title="View Roster" icon={Icon.Person} target={<Roster id={team.id} />} />
+          <Action.OpenInBrowser title="View Team on ESPN" url={team.link} />
+          <Action title="Back" />
+        </ActionPanel>
+      }
+    />
+  );
+};
+
+const ListTeamComponent = ({ team }: { team: Team }) => {
+  return (
+    <List.Item
+      key={team.id}
+      title={team.name}
+      icon={team.logo}
+      accessories={[{ text: `W: ${team.wins}` }, { text: `L: ${team.losses}` }]}
       actions={
         <ActionPanel>
           <Action.Push title="View Roster" icon={Icon.Person} target={<Roster id={team.id} />} />
@@ -22,6 +40,14 @@ const TeamComponent = ({ team }: PropTypes) => {
       }
     />
   );
+};
+
+const TeamComponent = ({ team, viewType }: PropTypes) => {
+  if (viewType === "list") {
+    return <ListTeamComponent team={team} />;
+  } else {
+    return <GridTeamComponent team={team} />;
+  }
 };
 
 export default TeamComponent;
--- src/views/standings.tsx
+++ src/views/standings.tsx
@@ -1,4 +1,4 @@
-import { Grid, Toast, showToast } from "@raycast/api";
+import { Grid, List, Toast, showToast, useNavigation } from "@raycast/api";
 import { Team } from "../types/standings.types";
 import useStandings from "../hooks/useStandings";
 import TeamComponent from "../components/Team";
@@ -6,25 +6,84 @@ import TeamComponent from "../components/Team";
 const Standings = () => {
   const data = useStandings();
 
+  const { push } = useNavigation();
+  const { pop } = useNavigation();
+
   if (data.error) {
     showToast(Toast.Style.Failure, "Failed to get roster");
     data.loading = false;
   }
 
-  return (
-    <Grid isLoading={data.loading} itemSize={Grid.ItemSize.Medium} inset={Grid.Inset.Medium}>
-      <Grid.Section title="Eastern Conference">
-        {data.standings.eastern.map((team: Team) => {
-          return <TeamComponent key={team.id} team={team} />;
-        })}
-      </Grid.Section>
-      <Grid.Section title="Western Conference">
-        {data.standings.western.map((team: Team) => {
-          return <TeamComponent key={team.id} team={team} />;
-        })}
-      </Grid.Section>
-    </Grid>
-  );
+  const ListView = () => {
+    return (
+      <List
+        isLoading={data.loading}
+        searchBarAccessory={
+          <List.Dropdown
+            tooltip="View Type"
+            storeValue={false}
+            onChange={(view) => {
+              if (view === "grid") {
+                push(<GridView />);
+              }
+            }}
+          >
+            <List.Dropdown.Item key="list" title="List" value="list" />
+            <List.Dropdown.Item key="grid" title="Grid" value="grid" />
+          </List.Dropdown>
+        }
+      >
+        <List.Section title="Eastern Conference">
+          {data.standings.eastern.map((team: Team) => {
+            return <TeamComponent key={team.id} team={team} viewType="list" />;
+          })}
+        </List.Section>
+        <List.Section title="Western Conference">
+          {data.standings.western.map((team: Team) => {
+            return <TeamComponent key={team.id} team={team} viewType="list" />;
+          })}
+        </List.Section>
+      </List>
+    );
+  };
+
+  const GridView = () => {
+    return (
+      <Grid
+        isLoading={data.loading}
+        itemSize={Grid.ItemSize.Medium}
+        inset={Grid.Inset.Medium}
+        searchBarAccessory={
+          <Grid.Dropdown
+            tooltip="View Type"
+            storeValue={false}
+            defaultValue="grid"
+            onChange={(view) => {
+              if (view === "list") {
+                push(<ListView />);
+              }
+            }}
+          >
+            <Grid.Dropdown.Item key="grid" title="Grid" value="grid" />
+            <Grid.Dropdown.Item key="list" title="List" value="list" />
+          </Grid.Dropdown>
+        }
+      >
+        <Grid.Section title="Eastern Conference">
+          {data.standings.eastern.map((team: Team) => {
+            return <TeamComponent key={team.id} team={team} viewType="grid" />;
+          })}
+        </Grid.Section>
+        <Grid.Section title="Western Conference">
+          {data.standings.western.map((team: Team) => {
+            return <TeamComponent key={team.id} team={team} viewType="grid" />;
+          })}
+        </Grid.Section>
+      </Grid>
+    );
+  };
+
+  return <GridView />;
 };
 
 export default Standings;
